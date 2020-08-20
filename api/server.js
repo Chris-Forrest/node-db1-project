70df9f58c8fcb0c35b550f7189d3c06b4e706c18
Chris Forrest <redrider_cr@yahoo.com>
2020-08-20 12:10:54 -0400

    delete post working added a logger and error handler middleware

--- api/server.js
+++ api/server.js
@@ -6,6 +6,18 @@ const db = require("../data/dbConfig.js");
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 server.use("/api/accounts",accountRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({ message: " Something went wrong"})
+});
+
+function logger( req, res, next){
+    console.log(`${new Date().toISOString()} ${req.ip} ${req.method} ${req.url}`);
+
+    next()
+};
 
 module.exports = server;
