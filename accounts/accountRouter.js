2964eec46bee8299257748c532886c3eb28732d9
Chris Forrest <redrider_cr@yahoo.com>
2020-08-20 11:18:42 -0400

    get accounts and get account by id working

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -7,10 +7,19 @@ const db = require("../data/dbConfig");
 
 router.get("/", async (req,res) => {
     try{
-        const acounts = await db.select("*").from("accounts")
+        const accounts = await db.select("*").from("accounts")
         res.status(200).json(accounts)
     }catch(err){
         res.status(500).json({ message: "Could not retrieve accounts."})
+    }
+})
+
+router.get("/:id", async (req,res) => {
+    try{
+        const [account] = await db.select("*").from("accounts").where("id",req.params.id).limit(1)
+        res.json(account)
+    }catch(err){
+        res.status(500).json({ message:"Could not retrieve account."})
     }
 })
 
--- api/server.js
+++ api/server.js
@@ -6,6 +6,6 @@ const db = require("../data/dbConfig.js");
 const server = express();
 
 server.use(express.json());
-server.use("/api",accountRouter);
+server.use("/api/accounts",accountRouter);
 
 module.exports = server;
